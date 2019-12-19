b9790860be0c9c5be35017cd5968691a70f555b8
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-20 02:35:33 +0700

    Cards-class developed, extends ADataSet-class, implements IDataSet-interface
    Contains two constructors and method openFile();
    Overrides methods updateById(), deleteById(), add();
    Implements methods save() and read();

---
+++ app/src/main/java/com/example/dontforget/model/Cards.java
@@ -0,0 +1,77 @@
+package com.example.dontforget.model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cards<T extends Card> extends ADataSet<T> implements IDataSet<T> {
+    private File file;
+    private final String fileNameToOpen = "dont_forget.db";
+    private File openFile() {
+
+
+        return null;
+    }
+
+    public Cards() {
+        itemsList = new ArrayList<>();
+        file = openFile();
+        load();
+    }
+
+    public Cards(List<T> cardsList) {
+        itemsList = cardsList;
+        save();
+    }
+
+    @Override
+    public void updateById(long id, T item) {
+        super.updateById(id, item);
+        file = openFile();
+        save();
+    }
+
+    @Override
+    public void deleteById(long id) {
+        super.deleteById(id);
+        save();
+    }
+
+    @Override
+    public void add(T item) {
+        super.add(item);
+        save();
+    }
+
+    @Override
+    public void save() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
+            out.writeObject(itemsList);
+        } catch (FileNotFoundException e) {
+            e.getStackTrace();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    };
+
+    @Override
+    public boolean load() {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
+            itemsList = (List<T>) in.readObject();
+            return true;
+        } catch (FileNotFoundException e) {
+            e.getStackTrace();
+        } catch (IOException e) {
+            e.getStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.getStackTrace();
+        }
+        return false;
+    }
+}
