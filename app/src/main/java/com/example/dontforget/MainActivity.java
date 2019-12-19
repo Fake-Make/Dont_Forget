e52c693d9dfa251c8952f51b562dbcfeaef47045
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-20 00:18:09 +0700

    Project created

---
+++ app/src/main/java/com/example/dontforget/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.dontforget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Shows cards list and button to add one
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
