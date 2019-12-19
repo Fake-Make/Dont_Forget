7c3f1b6fdd3ad7e842bd9a0e53ce789263cbed74
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-20 02:31:41 +0700

    Alarms-class developed, extends ADataSet-class, implements IDataSet-interface
    Contains two constructors and save/load mocks;

--- app/src/main/java/com/example/dontforget/model/Alarms.java
+++ app/src/main/java/com/example/dontforget/model/Alarms.java
@@ -1,62 +1,16 @@
 package com.example.dontforget.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Alarms<T extends Alarm> implements IDataSet<T> {
-    List<T> alarmsList;
-
+public class Alarms<T extends Alarm> extends ADataSet<T> implements IDataSet<T>, Serializable {
     public Alarms() {
-        alarmsList = new ArrayList<>();
+        itemsList = new ArrayList<>();
     }
 
     public Alarms(List<T> alarmsList) {
-        this.alarmsList = alarmsList;
-    }
-
-    @Override
-    public T getById(long id) {
-        for (T item : alarmsList) {
-            if (id == item.id)
-                return item;
-        }
-        return null;
-    }
-
-    public int searchById(long id) {
-        for (int i = 0; i < alarmsList.size(); i++) {
-            if (id == alarmsList.get(i).id)
-                return i;
-        }
-        return -1;
-    }
-
-    @Override
-    public void updateById(long id, T item) {
-        int existingItemIdx = searchById(id);
-        if (-1 == existingItemIdx)
-            return;
-
-        alarmsList.set(existingItemIdx, item);
-    }
-
-    @Override
-    public void deleteById(long id) {
-        int existingItemIdx = searchById(id);
-        if (-1 == existingItemIdx)
-            return;
-
-        alarmsList.remove(existingItemIdx);
-    }
-
-    @Override
-    public void add(T item) {
-        alarmsList.add(item);
-    }
-
-    @Override
-    public List<T> getList() {
-        return alarmsList;
+        itemsList = alarmsList;
     }
 
     @Override
@@ -65,7 +19,7 @@ public class Alarms<T extends Alarm> implements IDataSet<T> {
     }
 
     @Override
-    public void load() {
-        // Not available for Alarms
+    public boolean load() {
+        return false;
     }
 }
