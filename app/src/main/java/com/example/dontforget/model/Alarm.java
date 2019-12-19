617b9a2148974c2a14a368621f22f85309a55601
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-20 02:27:24 +0700

    Alarm-class developed, implements IUniqueItem
    Contains caption, getters and setters for it

--- app/src/main/java/com/example/dontforget/model/Alarm.java
+++ app/src/main/java/com/example/dontforget/model/Alarm.java
@@ -1,14 +1,16 @@
 package com.example.dontforget.model;
 
+import java.io.Serializable;
 import java.util.Calendar;
 
-public class Alarm implements IAlarm {
+public class Alarm implements IAlarm, IUniqueItem, Serializable {
     protected String caption;
     // Timestamps
     protected long nextAlarm, interval;
     protected long id;
 
     public Alarm(Calendar lastDate, Calendar nextDate, String time) {
+        id = System.currentTimeMillis();
         // nextAlarm = nextDate + time;
         // interval = nextAlarm - lastDate;
     }
@@ -19,5 +21,15 @@ public class Alarm implements IAlarm {
 
     public void setCaption(String caption) {
         this.caption = caption;
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
     }
 }
