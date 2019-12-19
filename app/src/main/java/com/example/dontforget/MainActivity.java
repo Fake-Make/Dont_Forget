64da469bc0d8a380983cd2407d6bf36b8139a2a8
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-20 02:35:53 +0700

    Logical TODO-notices added

--- app/src/main/java/com/example/dontforget/MainActivity.java
+++ app/src/main/java/com/example/dontforget/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dontforget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Shows cards list and button to add one
@@ -13,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // presenter.loadList
+    }
+
+    public void showList() {
+        // presenter.toAnotherActivity(this, )
     }
 }
