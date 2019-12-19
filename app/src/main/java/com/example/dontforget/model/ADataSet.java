921751ab2446c7d86b399d196690933d9bcfaa6e
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-20 02:30:17 +0700

    Abstract ADataSet-class developed, implements IDataSet-interface
    Implements getById(), searchById(), deleteById(), updateById(), isEmpty(), add() and getList() methods;

---
+++ app/src/main/java/com/example/dontforget/model/ADataSet.java
@@ -0,0 +1,58 @@
+package com.example.dontforget.model;
+
+import java.util.List;
+
+abstract public class ADataSet<T extends IUniqueItem> implements IDataSet<T> {
+    List<T> itemsList;
+
+    @Override
+    public T getById(long id) {
+        for (T item : itemsList) {
+            if (id == item.getId())
+                return item;
+        }
+        return null;
+    }
+
+    public int searchById(long id) {
+        for (int i = 0; i < itemsList.size(); i++) {
+            if (id == itemsList.get(i).getId())
+                return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public void deleteById(long id) {
+        int existingItemIdx = searchById(id);
+        if (-1 == existingItemIdx)
+            return;
+
+        itemsList.remove(existingItemIdx);
+    }
+
+    @Override
+    public void updateById(long id, T item) {
+        int existingItemIdx = searchById(id);
+        if (-1 == existingItemIdx)
+            return;
+
+        item.setId(id);
+        itemsList.set(existingItemIdx, item);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return 0 >= itemsList.size();
+    }
+
+    @Override
+    public void add(T item) {
+        itemsList.add(item);
+    }
+
+    @Override
+    public List<T> getList() {
+        return itemsList;
+    }
+}
