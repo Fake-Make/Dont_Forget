3f1d53e1cc590d3eb3a326d956562d046091bfeb
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-22 04:11:41 +0700

    Protected field parent of Context-type added

--- app/src/main/java/com/example/dontforget/presenter/APresenter.java
+++ app/src/main/java/com/example/dontforget/presenter/APresenter.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 
 public abstract class APresenter implements IPresenter {
+    protected Context parent;
     protected Cards<Card> cardsList;
 
     protected File getFile(Context context) {
