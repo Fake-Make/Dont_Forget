d589ac2e785796396fb154cec2b225e4205689b6
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-22 03:46:30 +0700

    APresenter-class changed: now it gets Context at constructor

--- app/src/main/java/com/example/dontforget/presenter/APresenter.java
+++ app/src/main/java/com/example/dontforget/presenter/APresenter.java
@@ -1,8 +1,17 @@
 package com.example.dontforget.presenter;
 
+import android.content.Context;
+
 import com.example.dontforget.model.Card;
 import com.example.dontforget.model.Cards;
 
+import java.io.File;
+import java.io.IOException;
+
 public abstract class APresenter implements IPresenter {
     protected Cards<Card> cardsList;
+
+    protected File getFile(Context context) {
+        return context.getFilesDir();
+    }
 }
