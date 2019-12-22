bbef77d64781df60aa8a3a8732ada0b0d1c8d42b
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-22 18:28:50 +0700

    View-logic added at CardActivity

--- app/src/main/java/com/example/dontforget/CardActivity.java
+++ app/src/main/java/com/example/dontforget/CardActivity.java
@@ -3,6 +3,10 @@ package com.example.dontforget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dontforget.presenter.CardPresenter;
 
 /**
  * Detailed card's page
@@ -10,10 +14,25 @@ import android.os.Bundle;
  * Also contains button to create new alarm
  */
 public class CardActivity extends AppCompatActivity {
+    protected final String errorMessageNoSuchCard = "There is no such card!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+
+        long id = 0;
+        String idAsString = getIntent().getStringExtra("CARD_ID");
+        if (null == idAsString) {
+            finish();
+            Toast.makeText(this, errorMessageNoSuchCard, Toast.LENGTH_SHORT).show();
+        } else {
+            id = Long.parseLong(idAsString);
+        }
+
+        CardPresenter presenter = new CardPresenter(CardActivity.this, id);
+        presenter.showCardInfo((TextView) findViewById(R.id.textViewCaption), (TextView) findViewById(R.id.textViewDescription));
+        presenter.switchEmptinessMessage((TextView) findViewById(R.id.emptinessMessageAlarms));
+        //presenter.showCardAlarms();
     }
 }
