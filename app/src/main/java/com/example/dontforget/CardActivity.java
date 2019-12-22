d43f61b8ddc7b8c5880bd7489d642393c6b2e90f
Dmitry Nevada <vegnerdima@yandex.ru>
2019-12-22 23:37:01 +0700

    Transferring Card's id into CreatingAlarmActivity added at CardActivity

--- app/src/main/java/com/example/dontforget/CardActivity.java
+++ app/src/main/java/com/example/dontforget/CardActivity.java
@@ -2,7 +2,10 @@ package com.example.dontforget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,13 +18,14 @@ import com.example.dontforget.presenter.CardPresenter;
  */
 public class CardActivity extends AppCompatActivity {
     protected final String errorMessageNoSuchCard = "There is no such card!";
+    protected long id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
 
-        long id = 0;
+        // Get card's id from intent
         String idAsString = getIntent().getStringExtra("CARD_ID");
         if (null == idAsString) {
             finish();
@@ -30,9 +34,21 @@ public class CardActivity extends AppCompatActivity {
             id = Long.parseLong(idAsString);
         }
 
+        // Draw card info and alarms list
         CardPresenter presenter = new CardPresenter(CardActivity.this, id);
         presenter.showCardInfo((TextView) findViewById(R.id.textViewCaption), (TextView) findViewById(R.id.textViewDescription));
         presenter.switchEmptinessMessage((TextView) findViewById(R.id.emptinessMessageAlarms));
+        // TODO: develop presenter.showCardAlarms() method
         //presenter.showCardAlarms();
+
+        // Set listener at add button
+        findViewById(R.id.buttonAddAlarm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CardActivity.this, CreatingAlarmActivity.class);
+                intent.putExtra("CARD_ID", String.valueOf(id));
+                startActivity(intent);
+            }
+        });
     }
 }
